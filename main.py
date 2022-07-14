a5a2b54a70cf8dbfdba66583dcbe8ed1cf140bfd
yabgo <yabgo99@gmail.com>
2022-07-14 22:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import base64
 
 secret = "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
 answer = base64.b64decode(secret)
-print(answer[::-1].decode())
+final_answer = answer[::-1].decode()
+
+print(final_answer)
